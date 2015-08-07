f4c86efffc3bef30262697fd60b1017a61661243
Kelli Margaret <kelli.sommerdyke@gmail.com>
2015-08-07 14:58:08 -0700

    Add final styling

--- app/app.php
+++ app/app.php
@@ -16,17 +16,24 @@
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__.'/../views'
     ));
-    /* */
+    /* Route to home page:
+        will display all contact information submitted through form fields
+        while also allowing users to add additional contacts to be saved*/
     $app->get("/", function() use($app) {
         return $app['twig']->render('contacts.html.twig', array('contacts' => Contact::getAll()));
     });
 
+    /* Route to created contact page:
+        appears when new contact has been created & saves contact in ther
+        list_of_contacts array */
     $app->post("/contacts", function() use ($app) {
         $contact = new Contact($_POST['name'], $_POST['phonenumber'], $_POST['address'], $_POST['email']);
         $contact->save();
         return $app['twig']->render('create_contact.html.twig', array('newcontact' => $contact));
     });
 
+    /* Route to confirmed delete contact page:
+        Deletes all previous contacts entered*/
     $app->post("/delete_contacts", function() use($app) {
         Contact::deleteAll();
         return $app['twig']->render('delete_contacts.html.twig');
