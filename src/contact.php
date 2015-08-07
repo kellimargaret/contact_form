c9808e9ee3cf86b59edc3c74e4b4ca74e59e2c8d
Kelli Margaret <kelli.sommerdyke@gmail.com>
2015-08-07 09:49:51 -0700

    Add getters and setters to contact.php

--- src/contact.php
+++ src/contact.php
@@ -11,22 +11,47 @@ class Contact
         $this->phonenumber = $phonenumber;
         $this->address = $address;
     }
+
     function setName($new_name)
     {
         $this->name = (string) $new_name;
     }
+
     function getName()
     {
         return $this->name;
     }
+
+    function setPhoneNumber($new_phonenumber)
+    {
+        $this->phonenumber = (string) $new_phonenumber;
+    }
+
+    function getPhoneNumber()
+    {
+        return $this->phonenumber;
+    }
+
+    function setAddress($new_address)
+    {
+        $this->address = (string) $new_address;
+    }
+
+    function getAddress()
+    {
+        return $this->address;
+    }
+
     function save()
     {
         array_push($_SESSION['list_of_contacts'], $this);
     }
+
     static function getAll()
     {
         return $_SESSION['list_of_contacts'];
     }
+
     static function deleteAll()
     {
         $_SESSION['list_of_contacts'] = array();
